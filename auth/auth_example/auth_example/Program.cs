8ac7ee61a7307da6b9b915ab43d6f81b78266d91
Randy Le <randyL04@hey.com>
2022-01-14 19:20:25 -0700

    fix login example, is finally working!

--- auth/auth_example/auth_example/Program.cs
+++ auth/auth_example/auth_example/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 {
     o.Authority = "http://localhost:3000";
     o.Audience = "api";
+    o.RequireHttpsMetadata = false;
 })
 ;
 
@@ -51,3 +52,55 @@ app.UseEndpoints(endpoints =>
 
 
 app.Run();
+
+
+/*
+ 
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+{
+    o.Authority = "http://localhost:3000";
+    o.Audience = "api";
+})
+;
+
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("Read", p => p.RequireClaim("scope", "api"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+
+app.Run();
+ */
