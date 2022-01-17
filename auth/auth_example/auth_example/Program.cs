c4293fb2c535a79611f56a8db1330136ad18053e
Randy Le <randyL04@hey.com>
2022-01-16 18:11:55 -0700

    asdf

--- auth/auth_example/auth_example/Program.cs
+++ auth/auth_example/auth_example/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,19 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddControllers();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
-{
-    o.Authority = "http://localhost:3000";
-    o.Audience = "api";
-    o.RequireHttpsMetadata = false;
-})
-;
-
-builder.Services.AddAuthorization(o =>
-{
-    o.AddPolicy("Read", p => p.RequireClaim("scope", "api"));
-});
+//builder.Services.AddAuthentication().AddOpenIdConnect("odic", o => { o.UsePkce = true; });
 
 var app = builder.Build();
 
@@ -65,6 +54,65 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 {
     o.Authority = "http://localhost:3000";
     o.Audience = "api";
+})
+;
+
+builder.Services.AddAuthorization(o =>
+{
+    o.AddPolicy("Read", p => p.RequireClaim("scope", "api"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+
+app.Run();
+ */
+
+// -----------------------------------
+
+/*
+ using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+{
+    o.Authority = "http://localhost:3000";
+    o.Audience = "api";
+    o.RequireHttpsMetadata = false;
 })
 ;
 
