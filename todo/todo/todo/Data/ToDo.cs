2d99a0dab0b362f4345266d0426498ed867bc7f4
Randy Le <randyL04@hey.com>
2022-01-25 10:20:06 -0700

    wip

--- todo/todo/todo/Data/ToDo.cs
+++ todo/todo/todo/Data/ToDo.cs
@@ -2,7 +2,11 @@
 {
     public class ToDo
     {
-        public string Summary { get; set; }
-        public bool IsCompleted { get; set; }
+        public const string TextUri = "http://schema.org/text";
+        public const string CreatedUri = "http://www.w3.org/2002/12/cal/ical#created";
+        public const string TypeUri = "http://www.w3.org/2002/12/cal/ical#Vtodo";
+
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
     }
 }
---
+++ todo/todo/todo/ToDoDocumentManager.cs
@@ -0,0 +1,9 @@
+﻿namespace todo
+{
+    /// <summary>
+    /// Handles the underlying RDF document stored at our pod
+    /// </summary>
+    public class ToDoDocumentManager
+    {
+    }
+}
